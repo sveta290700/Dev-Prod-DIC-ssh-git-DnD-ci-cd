9501a4a53735594d4d494e20973989f6e03117b0
sveta290700 <777rsb@mail.ru>
2021-11-15 21:42:18 +1000

    Scripts

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,2 +1,2 @@
 git push origin main
-ssh sammy@178.62.205.95 'cd /var/www/lab67_prod && cd ./scripts/build.sh'
+ssh -i ~/.ssh/id_rsa sammy@178.62.205.95 'cd /var/www/html/lab67_prod && cd ./scripts/build.sh'
