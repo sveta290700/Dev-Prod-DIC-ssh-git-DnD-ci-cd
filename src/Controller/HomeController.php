e000ae6ffe7fd47882a134dedec8510c6eb01551
sveta290700 <777rsb@mail.ru>
2021-11-16 16:17:31 +1000

    test prod deploy

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -42,7 +42,7 @@ class HomeController extends AbstractController
     public function deploy(): Response
     {
         return $this->json([
-            'message' => 'Successful dev deploy!'
+            'message' => 'Successful prod deploy!'
         ]);
     }
 }
