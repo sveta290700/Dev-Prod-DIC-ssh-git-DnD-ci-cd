646d171ec17d8b2f86e81bd73ce5a808d1f3a663
sveta290700 <777rsb@mail.ru>
2021-11-16 15:31:54 +1000

    deploy_prod script test

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "deploy_prod script test"
 git push origin master
-ssh sammy@178.62.205.95 'cd /var/www/html/lab67_dev/scripts && sh build.sh'
+ssh sammy@178.62.205.95 'cd /var/www/html/lab67_prod/scripts && sh build.sh'
